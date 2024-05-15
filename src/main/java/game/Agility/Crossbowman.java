3b410cd8e6e78aed6293af4317c338837931ee37
Alex B <alex.8ulavin@gmail.com>
2024-05-15 19:59:47 +0300

    Seminar 3 HomeWork --fixed troubles

--- src/main/java/game/Agility/Crossbowman.java
+++ src/main/java/game/Agility/Crossbowman.java
@@ -7,17 +7,21 @@ import java.util.ArrayList;
 public class Crossbowman extends Agility {
     protected int bolt;
     protected int accuracy;
+
     public Crossbowman(String name, int x, int y) {
         super(name, 120, 120, 25, 1, 15, 2, 1, 0, 40, x, y);
         this.bolt = 100;
         this.accuracy = 3;
     }
+
     public String getInfo() {
         return "Арбалетчик";
     }
+
     @Override
     public void step(ArrayList<BaseUnit> enemy, ArrayList<BaseUnit> friend) {
         if (getHP() <= 0 || bolt == 0) return;
         hitEnemy(findNearestTarget(enemy));
         bolt--;
+    }
 }
--- src/main/java/game/Strength/Piciner.java
+++ src/main/java/game/Strength/Piciner.java
@@ -6,7 +6,7 @@ public class Piciner extends Strength {
     protected int armor;
     public Piciner(String name, int x, int y) {
         super(name, 140, 140, 30, 3, 18, 3, 1, 0, x, y);
-        this.armor = armor;
+    this.armor = 150;
     }
     public String getInfo(){
         return "Пикинер";
